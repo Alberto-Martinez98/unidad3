3954806e733a5b4c19bbd1c3fe51e4f8d0f5b3fd
Heriberto <hdecossmendez@hotmail.com>
2021-05-31 02:19:54 -0500

    se agrego la vista principal

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('principal');
+});
+
 Route::get('/login', function () {
     return view('login');
 })->name('login')->middleware('guest');
