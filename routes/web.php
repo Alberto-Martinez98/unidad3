ce60042d46139cdde4afa201f5ebea0482e77939
vagrant <vagrant@homestead>
2021-06-03 21:15:27 +0000

    borre ruta

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/prueba', function () {
-
-	$user=Auth::user();
-	if($user->esSupervisor()){
-		echo "eres supervisor";
-	}else
-	{
-		echo "eres otro";
-	}
-
-
 
 
     return view('principal');
